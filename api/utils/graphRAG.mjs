f45853c9e6404e1d78b99ee1e38166cade2ab282
Forbes Li <stufedpand@gmail.com>
2024-11-17 14:56:23 -0500

    Added context prompt for summarizations

--- api/utils/graphRAG.mjs
+++ api/utils/graphRAG.mjs
@@ -23,12 +23,39 @@ export const getBioprojectsSummarization = async (bioprojects) => {
         `,
     );
     const serializedBioprojects = bioprojectRecords.map(serializeBioProjectRecord);
+
+    const context = `
+    You are a bioinformatician working on a project to capture and summarize bioprojects to be displayed on the page based on a set of filters.
+    You will follow the below pipeline in order to summarize projects:
+    1. The user will be give you a list of bioproject objects including their title, description and ID.
+    2. You will need to read through the projects and gain a comprehensive understanding of all the projects through the title and project description.
+    3. You should provide a succinct overview of the high-level ideas covered by the bioprojects, no longer than a paragraph.
+    4. For each topic in the summarization, you should mention the relevant bioproject ID(s).
+    5. You should not provide any specific details about the bioprojects, only a high-level summary.
+    6. Do NOT use bioprojects that aren't given in the list.
+    7. Make sure you ONLY use the information provided in the bioprojects to generate the summary.
+    8. Avoid using any external information or knowledge.
+    9. You may be asked to re-evaluate your answer and given hints or tips to alter your previous answer.
+    `;
+
+    let model, role;
+    model = 'gpt4o';
+    if (model === 'gpt4o') {
+        role = 'system';
+    } else {
+        role = 'assistant'; // o1
+    }
+
     const prompt = [
         {
+            role: role,
+            content: context,
+        },
+        {
             role: 'user',
-            content: `Please provide a brief integrated summarization of the following BioProjects: \n ${serializedBioprojects}`,
+            content: `Please provide a brief overview of the following bioprojects: \n ${serializedBioprojects}`,
         },
     ];
-    const result = await runLLMCompletion(prompt);
+    const result = await runLLMCompletion(prompt, model);
     return result;
 };
